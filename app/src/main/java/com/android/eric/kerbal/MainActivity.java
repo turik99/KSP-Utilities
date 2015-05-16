af4a7ccf789f98db93838370c97f2f8de8210bcf
Eric <eric.zaba@gmail.com>
2015-05-16 02:11:06 -0500

    Crazy tunes, hanging out the window

--- app/src/main/java/com/android/eric/kerbal/MainActivity.java
+++ app/src/main/java/com/android/eric/kerbal/MainActivity.java
@@ -47,9 +47,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(getApplicationContext(), paracalc.class);
                     startActivity(intent);
                 }
-                if (position == 2){
 
-                }
 
             }
         });
